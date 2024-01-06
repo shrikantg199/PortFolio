740853a51a244d1ae05ae762f3024d7371635ed2
Shrikant  Gaikwad <131657065+shrikantg199@users.noreply.github.com>
2024-01-06 18:36:41 +0530

    add animation

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,20 +3,38 @@ import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import { FaUserTie } from "react-icons/fa";
 import { MdOutlineDocumentScanner } from "react-icons/md";
-
+import TextTransition, { presets } from "react-text-transition";
+import React from "react";
+const TEXTS = [
+  "Full Stack Web Developer.",
+  "Mern Stack Developer.",
+  "Thank You For Visiting.",
+];
 const Hero = () => {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      1500 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
   return (
-    <div className="py-16 min-h-[80vh] md:min-h-[85vh] flex items-center flex-col-reverse lg:flex-row gap-10 justify-center">
-      <div className="flex flex-col gap-4 mx-6 text-left md:gap-6 lg:w-1/2 2xl:w-1/3 xl:mx-0 ">
-        <p className="text-3xl font-bold text-light-blue-900">Hey,</p>
+    <div className="py-16 min-h-[80vh] md:min-h-[85vh] flex items-center flex-col-reverse lg:flex-row gap-4 justify-center">
+      <div className="flex flex-col gap-4 mx-6 text-left md:gap-6 2xl:w-1/3 xl:mx-0 ">
+        <p className="text-3xl font-bold text-light-blue-500">Hey,</p>
         <p className="relative text-3xl font-bold md:text-5xl">
           I'm
-          <span className="p-1 ml-4 uppercase text-light-blue-500">
-            Shrikant Gaikwad
-          </span>
+          <span className="m-2 uppercase text-light-blue-500">Shrikant</span>
+          <span className="m-2 uppercase text-light-blue-500">Gaikwad</span>
         </p>
-        <p className="relative text-xl font-semibold uppercase">
-          Full stack Developer.
+        <p className="relative text-2xl font-black uppercase text-[#6b21a8] ">
+          <h1>
+            <TextTransition springConfig={presets.wobbly}>
+              {TEXTS[index % TEXTS.length]}
+            </TextTransition>
+          </h1>
         </p>
         <p className="relative text-base font-medium md:text-xl">
           Enthusiastic fresher MERN stack developer equipped with skills in
@@ -37,7 +55,7 @@ const Hero = () => {
               About Me
             </Button>
           </Link>
-          <Link to="https://drive.google.com/file/d/1MRh3gOHNifyoATrYpG5BoZG5wQtl4co7/view?usp=sharing" target="_blank">
+          <Link to="#addresumelink" target="_blank">
             <Button
               variant="gradient"
               color="light-blue"
@@ -53,7 +71,7 @@ const Hero = () => {
         <img
           src={photo}
           alt="Shrikant Gaikwad"
-          className="object-cover object-center ml-12 rounded-full cursor-pointer w-52 h-52 md:w-72 md:h-72 hover:shadow-lg"
+          className="object-cover object-center ml-20 rounded-full cursor-pointer w-52 h-52 md:w-72 md:h-72 hover:shadow-lg"
         />
       </div>
     </div>
