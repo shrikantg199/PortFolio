3f22a3c7a53e10cc94e30b2e54df1e0fc6d36bfd
Shrikant  Gaikwad <131657065+shrikantg199@users.noreply.github.com>
2024-01-14 18:41:08 +0530

    little change in  about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,31 +3,28 @@ import React from "react";
 const About = () => {
   return (
     <div className="flex flex-col items-center justify-center pt-2 pb-10 md:flex-row">
-      <div className="p-6 text-justify md:w-1/2">
+      <div className="p-6 font-serif text-justify md:w-1/2">
         <h1 className="mt-2 mb-5 text-2xl font-bold text-center border-b-2 border-light-blue-500">
           About Me
         </h1>
         <p className="text-lg md:text-xl">
-          I am a passionate <b>Full Stack web Developer & MERN Dev.</b> with
-          hands on experience in building innovative and scalable web
-          applications.
+          My Name is <b>Shrikant Gaikwad</b>. I am an electronics &
+          communication engineering graduate with expertise in{" "}
+          <b>Full-stack web development.</b> He has hands-on experience building
+          responsive applications using{" "}
+          <b>JavaScript, React, Node.js and MongoDB.</b>
           <br />
+          <br />I have demonstrated strong technical skills including{" "}
+          <b>frontend development</b> with React, backend development with
+          Node.js and Express, and database management with MongoDB. I am adept
+          at crafting intuitive user interfaces and optimizing performance.
           <br />
-          My Educational background includes a Bachelor of Engineering degree in
-          Electronic and telecommunication Engineering from Pune,Maharashtra
+          <br />I have spearheaded innovative projects such as a{" "}
+          <b>real-time chat room </b>
+          web app that increased user engagement 40% and reduced login times
+          30%. I also built a <b>MERN job portal </b> that streamlined the job
+          search process and boosted user engagement 40%.
           <br />
-          <br />
-          Results-oriented recent graduate in Java and JavaScript, specializing
-          in full-stack development with proficiency in HTML5, CSS3, Node.js,
-          Express.js, MongoDB, and React. Adept at translating conceptual ideas
-          into tangible, user-friendly applications.
-          <br />
-          <br />
-          Solid foundation in software testing, version control, and database
-          management. Possesses strong communication, teamwork, and creative
-          problem-solving skills. Eager to leverage academic knowledge and
-          practical experience to contribute to dynamic software projects as a
-          dedicated full-stack developer.
         </p>
       </div>
     </div>
